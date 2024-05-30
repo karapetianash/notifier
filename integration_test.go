db109d3a8233196d281de57fbc4a6e595bdb680a
Ashot Karapetian <AshotArtashesi@yandex.ru>
2024-05-30 18:29:48 +0300

    add integration test for notification package

---
+++ integration_test.go
@@ -0,0 +1,19 @@
+//go:build integration
+
+package notify_test
+
+import (
+	"testing"
+
+	"notify"
+)
+
+func TestSend(t *testing.T) {
+	n := notify.New("test title", "test msg", notify.SeverityNormal)
+
+	err := n.Send()
+
+	if err != nil {
+		t.Error(err)
+	}
+}
